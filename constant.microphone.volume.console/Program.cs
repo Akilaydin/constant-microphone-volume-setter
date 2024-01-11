fa2bc6b505ab9f12cdd67dd6a6911fe67fcf220c
Artem Ovchinnikov <artem.o@bimiboo.com>
2024-01-11 19:32:22 +0300

    Created program to keep the microphone volume constant at 100%

--- constant.microphone.volume.console/Program.cs
+++ constant.microphone.volume.console/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace constant.microphone.volume.console;
+﻿using System.Globalization;
+using System.Timers;
 
-class Program
+using NAudio.CoreAudioApi;
+
+using Timer = System.Timers.Timer;
+
+namespace constant.microphone.volume.console;
+
+internal class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	private const float VolumeToSet = 1f;
+	
+	private static void Main(string[] args)
+	{
+		StartMicrophoneTimer();
+
+		Console.ReadLine();
+	}
+	
+	private static void StartMicrophoneTimer()
+	{
+		var timer = new Timer(1000);
+		timer.AutoReset = true;
+		timer.Elapsed += SetVolume;
+		timer.Start();
+	}
+
+	private static void SetVolume(object? sender, ElapsedEventArgs e)
+	{
+		SetCurrentVolumeText();
+
+		var microphone = GetCurrentMicrophone();
+
+		microphone.AudioEndpointVolume.MasterVolumeLevelScalar = VolumeToSet;
+	}
+
+	private static void SetCurrentVolumeText()
+	{
+		var microphone = GetCurrentMicrophone();
+
+		var currentVolumeString = (microphone.AudioEndpointVolume.MasterVolumeLevelScalar * 100.0f).ToString(CultureInfo.InvariantCulture);
+		
+		Console.WriteLine($"Volume of the current microphone {microphone.FriendlyName} is {currentVolumeString}%");
+	}
+
+	private static MMDevice GetCurrentMicrophone()
+	{
+		var enumerator = new MMDeviceEnumerator();
+
+		var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+
+		return devices[0];
+	}
 }
